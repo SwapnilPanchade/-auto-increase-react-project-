f9683c7c158cadd044b3fece1682359cafe3e288
SwapnilPanchade <sspanchade113@gmail.com>
2025-05-19 11:14:34 +0530

    used useref to store the ref of a state and update based on the state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Timeout = any;
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
+  const intervelRef = useRef<null | string | number | Timeout | undefined>(
+    null
+  );
   useEffect(() => {
-    const intervel = setInterval(() => {
-      setCount((prevCount) => prevCount + 1);
-    }, 1000);
+    startCounter();
 
-    // unmount the intervel
-    return () => clearInterval(intervel);
+    return () => clearInterval(intervelRef.current);
   }, []);
 
+  const startCounter = () => {
+    if (intervelRef.current === null) {
+      intervelRef.current = setInterval(() => {
+        setCount((prevCounter) => prevCounter + 1);
+      }, 1000);
+    }
+  };
+
+  const pauseCounter = () => {
+    clearInterval(intervelRef.current);
+    intervelRef.current = null;
+  };
+
+  const resumeCounter = () => {
+    startCounter();
+  };
   return (
     <>
       <h1>This is auto counter increaser {count}</h1>
@@ -18,17 +37,12 @@ function App() {
         {" "}
         Pause{" "}
       </button>
-      <button> Resume </button>
+      <button style={{ padding: 1, margin: 5 }} onClick={resumeCounter}>
+        {" "}
+        Resume{" "}
+      </button>
     </>
   );
-}
-
-const pauseCounter = () => {
-  setCount((prev) => prev);
-};
-
-const resumeCounter = () => {
-  
 }
 
 export default App;
